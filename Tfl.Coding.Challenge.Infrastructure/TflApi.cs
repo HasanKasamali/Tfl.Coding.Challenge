e4f8d1adb8581c9a9612544f402b100cf2914bef
Hasan Kasamali <KasamaliHasan@yahoo.co.uk>
2022-02-09 18:39:04 +0000

    Implement actual service call to TFL api and write integration tests

---
+++ Tfl.Coding.Challenge.Infrastructure/TflApi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace Tfl.Coding.Challenge.Infrastructure
+{
+    public class TflApi : ITflApi
+    {
+
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _config;
+
+        public TflApi (IConfiguration config)
+        {
+            _config = config;
+
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri (_config["BaseUrl"]);
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+        }
+
+        public async Task<RoadStatusModel> Status(string id, CancellationToken token = default)
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, new Uri($"road/{id}?app_id={_config["AppId"]}&app_key={_config["AppKey"]}", UriKind.Relative));
+            HttpResponseMessage response = await _httpClient.SendAsync(request, token);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var roadStatusModel = response.Content.ReadAsStringAsync().Result;
+                var result = JsonConvert.DeserializeObject<List<RoadStatusModel>>(roadStatusModel);
+                result[0].SuccessCode = response.StatusCode.ToString();
+                return result[0];
+            }
+            else
+            {
+                return new RoadStatusModel { Id= id, SuccessCode = response.StatusCode.ToString()};
+            }
+        }
+    }
+}
---
+++ Tfl.Coding.Challenge.Tests/IntegrationTests.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+using Moq;
+using Tfl.Coding.Challenge.Infrastructure;
+using Microsoft.Extensions.Configuration;
+
+namespace Tfl.Coding.Challenge.Tests
+{
+    public class IntegrationTests
+    {
+        private readonly TflApi _sut;
+        private readonly Mock<IConfiguration> _config;
+
+        public IntegrationTests()
+        {
+            _config = new Mock<IConfiguration>();
+            _config.SetupGet(x => x[It.Is<string>(s => s == "BaseUrl")]).Returns("https://api.tfl.gov.uk/");
+            _config.SetupGet(x => x[It.Is<string>(s => s == "AppId")]).Returns("testing_feb2022");
+            _config.SetupGet(x => x[It.Is<string>(s => s == "AppKey")]).Returns("cc8f576f5a3a4a1795a278eb7118003c");
+
+            _sut = new TflApi(_config.Object);
+        }
+
+        [Fact]
+        public void When_Valid_Road_Return_Success ()
+        {
+            // Arrange
+
+            // Act
+            var actual = _sut.Status("A10", default).Result;
+
+            // Assert
+            Assert.True(actual.Id == "a10");
+            Assert.True(actual.SuccessCode == "OK");
+            Assert.True(actual.Url != null);
+        }
+
+        [Fact]
+        public void When_InValid_Road_Return_Success()
+        {
+            // Arrange
+
+            // Act
+            var actual = _sut.Status("A6", default).Result;
+
+            // Assert
+            Assert.True(actual.SuccessCode != "OK");
+            Assert.True(actual.Url == null);
+        }
+    }
+}
