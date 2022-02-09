cd36cbc17c0878fc4f49f2a85adcd5e7ca7b0c32
Hasan Kasamali <KasamaliHasan@yahoo.co.uk>
2022-02-09 17:35:57 +0000

    Demonstrate TDD Add test project. Create base model and write tests. The test will fail

---
+++ Tfl.Coding.Challenge/TflModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tfl.Coding.Challenge
+{
+    public class TflModel
+    {
+        public string Id { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Bounds { get; set; }
+
+        public string Envelope { get; set; }
+
+        public string Url { get; set; }
+    }
+}
